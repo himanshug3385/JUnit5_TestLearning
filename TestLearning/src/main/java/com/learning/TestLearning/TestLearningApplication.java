6b7ee950ad26aaf31b5eabc6088f645897b293f7
himanshug3385 <himanshugupta3385@gmail.com>
2024-09-16 12:47:48 +0530

    initial-commit

---
+++ TestLearning/src/main/java/com/learning/TestLearning/TestLearningApplication.java
@@ -0,0 +1,13 @@
+package com.learning.TestLearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestLearningApplication.class, args);
+	}
+
+}
---
+++ TestLearning/src/test/java/com/learning/TestLearning/TestLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.TestLearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
